69631887e3f0cabea86913fd548e6735d6412bcd
zanoniFab <fabianezanoni@gmail.com>
2023-04-05 21:11:53 -0300

    implementa formulario de cadastro com validacoes e corrige pasta server

--- client/src/components/FormHook/FormHook.jsx
+++ client/src/components/FormHook/FormHook.jsx
@@ -1,85 +1,79 @@
 import {useForm} from 'react-hook-form';
+import InputGroup from '../InputGroup';
+import {yupResolver} from '@hookform/resolvers/yup';
+import {validationSchema, defaultValues} from './FormSchema';
+import { useState } from 'react';
+import { apiService } from '../../service/api';
+import { useNavigate } from 'react-router-dom';
 function FormHook () {
+    const [isSubmitting, setSubmitting] = useState(false);
+    const navigate = useNavigate();
+    const {register,
+            handleSubmit,
+            formState:{errors},
+            reset} = useForm({resolver: yupResolver(validationSchema),defaultValues});
 
-    const {register, handleSubmit, formState:{errors}, reset} = useForm();
-
-    const onSubmit = (data)=> {
-        console.log(data);
-        reset();
+    const onSubmit = async (data)=> {
+        setSubmitting(true);
+        const payload = {...data};
+        delete payload.passwordConfirm;
+        const response = await apiService.post("/register",payload);
+        if (response.data) {
+            navigate("/login");
+            return;
+        }
+        setSubmitting(false);
+        alert(response.error);
     }
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-                <label htmlFor='nome'>Nome</label>
-                <input 
-                    id='nome'
-                    type='text'
-                    {...register('nome',{required: 'Nome obrigatório'})}
-                />
-                {errors.nome&&<span>{errors.nome.message}</span>}
-            </div>
-            <div> 
-                <label htmlFor='telefone'>telefone</label>
-                <input 
-                    id='telefone' 
-                    type='number'
-                    {...register('telefone')}
-                />
-            </div>
-            <div>
-                <label htmlFor='dataNascimento'>Data de Nascimento</label>
-                <input 
-                    id='dataNascimento' 
-                    type='date'
-                    {...register('dataNascimento',{required: 'Campo obrigatório'})}
-                />
-                {errors.dataNascimento&&<span>{errors.dataNascimento.message}</span>}
-            </div>
-            <div>
-                <label htmlFor='cpf'>Cpf</label>
-                <input 
-                    id='cpf' 
-                    type='number'
-                    {...register('cpf',{required: 'Campo obrigatório'})}
-                />
-                {errors.cpf&&<span>{errors.cpf.message}</span>}
-            </div>
-            <div>
-                <label htmlFor='email'>E-mail</label>
-                <input 
-                    id='email' 
-                    type='email'
-                    {...register('email',{required: 'Campo obrigatório'})}
-                />
-                {errors.email&&<span>{errors.email.message}</span>}
-            </div>
-            <div>
-                <label htmlFor='senha'>Senha</label>
-                <input 
-                    id='senha' 
-                    type='password'
-                    {...register('senha',{
-                        required: 'Campo obrigatório',
-                        minLength: {value:8,message:'A senha deve conter no mínimo 8 caracteres'},
-                    })}
-                />
-                {errors.senha&&<span>{errors.senha.message}</span>}
-            </div>
-            <div>
-                <label htmlFor='senhaRepeat'>Confirme sua senha</label>
-                <input 
-                    id='senhaRepeat' 
-                    type='password'
-                    {...register('senhaRepeat',{
-                        required: 'Campo obrigatório',
-                        minLength: {value:8,message:'A senha deve conter no mínimo 8 caracteres'},
-                    })}
-                />
-                {errors.senhaRepeat&&<span>{errors.senhaRepeat.message}</span>}
-            </div>
+            <InputGroup 
+                id='nome' 
+                type='text' 
+                labelText='nome'
+                {...register('nome')}
+                helperText = {errors?.nome?.message}
+            />              
+            <InputGroup 
+                id='telefone'
+                type='number' 
+                labelText='telefone' 
+                {...register('telefone')}
+            />
+            <InputGroup 
+                id='dataNascimento'
+                type='date'
+                labelText='dataNascimento'
+                {...register('dataNascimento')}
+                helperText = {errors?.dataNascimento?.message}
+            />
+            <InputGroup 
+                id='cpf' 
+                type='number'
+                {...register('cpf')}
+                helperText = {errors?.cpf?.message}
+            />
+            <InputGroup
+                id='email' 
+                type='text'
+                {...register('email')}
+                helperText={errors?.email?.message}
+            />
+            <InputGroup
+                id='password' 
+                type='password'
+                {...register('password')}
+                helperText={errors?.password?.message}
+            />
+            <InputGroup
+                id='passwordConfirm' 
+                type='password'
+                {...register('passwordConfirm')}
+                helperText={errors?.passwordConfirm?.message}
+            />
             <button type='button' onClick={()=>reset()}>Limpar</button>
-            <button type='submit'>Enviar</button>
+            <button type='submit'disabled={isSubmitting}>Enviar</button>
         </form>
     )
 }
---
+++ client/src/components/FormHook/FormSchema.js
@@ -0,0 +1,22 @@
+import * as Yup from 'yup';
+
+export const validationSchema = Yup.object(
+    {
+        nome: Yup.string().required('Nome obrigatório'),
+        email: Yup.string().email('E-mail inválido').required('E-mail obrigatório'),
+        password: Yup.string().required('Senha obrigatória').min(8,'A senha deve conter no mínimo 8 caracteres'),
+        passwordConfirm: Yup.string()
+            .required('Campo obrigatório')
+            .oneOf([Yup.ref('password')], 'A senhas não conferem'),
+    });
+
+export const defaultValues = {
+    nome: 'zanonao',
+    telefone: '',
+    dataNascimento: '',
+    cpf: '',
+    email: 'zanonao@gmail.com',
+    password: '12345678',
+    passwordConfirm: '12345678'
+}
+
