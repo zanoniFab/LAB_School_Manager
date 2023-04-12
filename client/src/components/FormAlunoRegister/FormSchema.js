447d1131681a9256b4cec90e99101e7fd1f848ad
zanoniFab <fabianezanoni@gmail.com>
2023-04-12 16:36:01 -0300

    Adiciona validacao do yup para todos os campos

--- client/src/components/FormAlunoRegister/FormSchema.js
+++ client/src/components/FormAlunoRegister/FormSchema.js
@@ -3,16 +3,16 @@ import * as Yup from 'yup';
 export const validationSchema = Yup.object(
     {
         nome: Yup.string().required('Nome obrigatório'),
+        telefone: Yup.string().required('Campo obrigatório'),
+        dataNascimento: Yup.string().required('Campo obrigatório'),
         cpf: Yup.string().required('Campo obrigatório'),
-        nota: Yup.number().required('Campo obrigatório'),
-        
+        nota: Yup.number().required('Campo obrigatório'),    
     });
 
 export const defaultValues = {
     nome: 'Fabiane',
-    telefone: '',
+    telefone: '999999999',
     dataNascimento: '',
     cpf: '8470606634',
-    nota: 5,
+    nota: 10,
 }
-
